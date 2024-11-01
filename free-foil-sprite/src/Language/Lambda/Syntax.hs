b611c2fd481ec718a0af16a614050b256184263b
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2024-11-01 12:06:16 +0300

    Add conversions, Show, IsString for Term

--- free-foil-sprite/src/Language/Lambda/Syntax.hs
+++ free-foil-sprite/src/Language/Lambda/Syntax.hs
@@ -1,17 +1,27 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveTraversable #-}
 module Language.Lambda.Syntax where
 
 -- import Control.Monad.Free.Foil
+import Data.String (IsString(..))
+import qualified Data.Map as Map
+import qualified Language.Lambda.Syntax.Print as Raw
+import qualified Language.Lambda.Syntax.Par as Raw
 import Data.Bifunctor.TH
 import qualified Control.Monad.Foil as Foil
 import Control.Monad.Free.Foil.TH.MkFreeFoil
 import Language.Lambda.FreeFoilConfig (lambdaConfig)
 
 mkFreeFoil lambdaConfig
+
 deriveBifunctor ''TermSig
 deriveBifoldable ''TermSig
 deriveBitraversable ''TermSig
@@ -25,6 +35,25 @@ instance Foil.CoSinkable Pattern where
     withBinder scope binder $ \f' binder' ->
       cont f' (PatternVar binder')
 
+mkFreeFoilConversions lambdaConfig
+
 type Expr = Term
 type Type = Term
 type Predicate = Term
+
+-- >>> "(x) => { let x = y; y }" :: Expr Foil.VoidS
+-- (x0) =>
+-- {
+--   let x0 = x1;
+--   x1
+-- }
+instance Show (Term n) where
+  show = Raw.printTree . fromTerm
+
+instance IsString (Term Foil.VoidS) where
+  fromString = toTerm Foil.emptyScope Map.empty . unsafeParseTerm
+    where
+      unsafeParseTerm input =
+        case Raw.pTerm (Raw.myLexer input) of
+          Left err -> error err
+          Right term -> term
