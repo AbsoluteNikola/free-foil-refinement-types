4b8c6c537d96b891f6882267935f012a2e091dd4
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2024-11-01 11:58:13 +0300

    Sketch typechecking

--- free-foil-sprite/src/Language/Lambda/Syntax.hs
+++ free-foil-sprite/src/Language/Lambda/Syntax.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_GHC -ddump-splices #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE PatternSynonyms #-}
@@ -7,7 +6,25 @@
 module Language.Lambda.Syntax where
 
 -- import Control.Monad.Free.Foil
+import Data.Bifunctor.TH
+import qualified Control.Monad.Foil as Foil
 import Control.Monad.Free.Foil.TH.MkFreeFoil
 import Language.Lambda.FreeFoilConfig (lambdaConfig)
 
 mkFreeFoil lambdaConfig
+deriveBifunctor ''TermSig
+deriveBifoldable ''TermSig
+deriveBitraversable ''TermSig
+
+instance Foil.CoSinkable Pattern where
+  coSinkabilityProof rename (PatternVar binder) cont =
+    Foil.coSinkabilityProof rename binder $ \rename' binder' ->
+      cont rename' (PatternVar binder')
+
+  withPattern withBinder _id _comp scope (PatternVar binder) cont =
+    withBinder scope binder $ \f' binder' ->
+      cont f' (PatternVar binder')
+
+type Expr = Term
+type Type = Term
+type Predicate = Term
